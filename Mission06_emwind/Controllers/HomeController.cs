f04586b5dd911a0db80a0600301412e53bf11a88
Emily Reyes <emilymwind1@gmail.com>
2023-02-13 22:48:18 -0700

    Add project files.

---
+++ Mission06_emwind/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_emwind.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_emwind.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DatingApplication() 
+        {
+            return View();
+         }
+
+        [HttpPost]
+        public IActionResult DatingApplication(ApplicationResponse ar)
+        {
+            return View("Confirmation", ar);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_emwind/Models/ApplicationResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_emwind.Models
+{
+    public class ApplicationResponse
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public byte Age { get; set; }
+        public string Phone { get; set; }
+        public string Major { get; set; }
+        public bool CreeperStalker { get; set; }
+    }
+}
